23a74b1ea7b0480f07e5767fcb0ec1465cdab575
Gaurav Das <gaurav2016das@gmail.com>
2021-05-16 16:21:32 +0530

    jar file modified

--- src/com/practice/spring/controller/EmployeeController.java
+++ src/com/practice/spring/controller/EmployeeController.java
@@ -1,12 +1,37 @@
 package com.practice.spring.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.portlet.ModelAndView;
+
+import com.practice.spring.beans.Employee;
+import com.practice.spring.services.EmployeeServiceImpl;
+
+@RestController
 public class EmployeeController {
 
-	ModelAndView loadLoginPage() {
-		return null;
+	@Autowired
+	private EmployeeServiceImpl employeeService;
+
+	@RequestMapping(value = "/loadForm", method = RequestMethod.GET)
+	public ModelAndView loadLoginPage() {
+		return new ModelAndView("employeeForm", "employeeBean", new Employee());
 	}
 
+	@RequestMapping(value = "/findById/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Employee>> findById(@PathVariable("id") Integer id) throws Exception {
+		List<Employee> listEmp = employeeService.findById(id);
+		if (listEmp == null)
+			return new ResponseEntity<List<Employee>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		else
+			return new ResponseEntity<List<Employee>>(listEmp, HttpStatus.OK);
+	}
 }
--- src/com/practice/spring/services/EmployeeService.java
+++ src/com/practice/spring/services/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.practice.spring.services;
 
+import java.util.List;
+
 import com.practice.spring.beans.Employee;
 
 public interface EmployeeService {
 	public int addEmployee(Employee employee) throws Exception;
+
+	public List<Employee> findById(int id) throws Exception;
 }
